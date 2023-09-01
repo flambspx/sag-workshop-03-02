3d79a8adae195b255fb5025876891d278575f6f7
Francesca Lamberti <143492490+flambspx@users.noreply.github.com>
2023-09-01 07:59:37 +0000

    no message

--- buildScripts/setEnv.sh
+++ buildScripts/setEnv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Pipeline parameters
-export MY_AZ_ACR_URL="azdevopspj1acr.azurecr.io"
-export AZ_BASE_IMAGE_REPO="msr-1011-lean-original-recipe"
-export AZ_BASE_IMAGE_FIXES_TAG="Fixes_22-09-06"
+export MY_AZ_ACR_URL="sxw4acr.azurecr.io"
+export AZ_BASE_IMAGE_REPO="msr-1015-lean-original-recipe"
+export AZ_BASE_IMAGE_FIXES_TAG="Fixes_23-08-30"
 export AZ_BASE_IMAGE_TAG="${MY_AZ_ACR_URL}/${AZ_BASE_IMAGE_REPO}:${AZ_BASE_IMAGE_FIXES_TAG}"
 
 export AZ_ACR_REPO_NAME="my-news-service-01"
